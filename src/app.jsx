e777945689c54c97b886ec0c972e6eb97b46bbc2
Zakaria177 <zrkraihan@gmail.com>
2025-01-29 11:32:40 +0600

    updated

--- src/app.jsx
+++ src/app.jsx
@@ -8,6 +8,16 @@ import Card from "./components/Card";
 import Data from "./data.json"
 
 export function App() {
+
+  // // mapping items using for loop 
+  // let items = [];
+  // for (let i=0 ; i <Data.length; i ++){
+  //    items.push (<Card tittleText = {Data[i].tittle} cartDes= { Data[i].textDes} />)
+  // }
+
+
+    //  Data.map((item) => <Card tittleText = {item.tittle} cartDes ={item.textDes} />  );
+
   return (
     <>
       <div>
@@ -16,10 +26,17 @@ export function App() {
         <Card tittleText ="Mail Hr" cartDes= "About the interview" />
         <Card tittleText ="Finsih 1 hr " cartDes= "From video 9 to 1 atleast 15" /> */}
 
-        <Card tittleText = {Data[0].tittle}cartDes= {Data [0].textDes} />
+        {/* MAPING DATA TO COMPONENT USING LINE BY LINE  */}
+      
+        {/* <Card tittleText = {Data[0].tittle}cartDes= {Data [0].textDes} />
         <Card tittleText = {Data[1].tittle}cartDes= {Data [1].textDes} />
         <Card tittleText = {Data[2].tittle}cartDes= {Data [2].textDes} />
-        <Card tittleText = {Data[3].tittle}cartDes= {Data [3].textDes} />
+        <Card tittleText = {Data[3].tittle}cartDes= {Data [3].textDes} /> */}
+
+        {/* {items} */} 
+
+        {Data.map((item,index ) => <Card key ={index} tittleText = {item.tittle} cartDes ={item.textDes} />  )}
+        
       </div>
     </>
   );
